2182eda510d62f4b4228f8b0e229c7cc759c86be
Phill Damaskin <philldamaskin@gmail.com>
2018-08-18 02:17:12 -0400

    bigger wave curve drawers

--- Assets/Code/Synthesizer/Editor/EditorCurveDrawer.cs
+++ Assets/Code/Synthesizer/Editor/EditorCurveDrawer.cs
@@ -8,12 +8,11 @@ public class EditorClampedCurveAttribute : PropertyDrawer
 {
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        if (property.hasMultipleDifferentValues)
-        {
-            EditorGUI.LabelField(position, label.text, "---");
-            return;
-        }
-
         EditorGUI.CurveField(position, property, Color.yellow, new Rect(0, 0, 1, 1));
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return 30;
     }
 }
